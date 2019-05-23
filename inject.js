e1fda92e8a2f74ff8f215024f2fad89386793b36
Federico-Ciuffardi <Federico.Ciuffardi@outlook.com>
2019-05-23 01:13:29 -0300

    deleted original inject

--- inject.js
+++
@@ -1,8 +0,0 @@
-var actualCode = `window.postMessage({
-	secs:document.querySelector('.video-stream').getCurrentTime()
-}, '*');
-`;
-var script = document.createElement('script');
-script.textContent = actualCode;
-(document.head||document.documentElement).appendChild(script);
-script.remove();
